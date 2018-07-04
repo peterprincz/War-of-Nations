92fefc0d1d6a8a0a0759aafe70a0883276d9c33e
godofdeath11 <ezmegaz11@gmail.com>
2018-07-04 14:53:00 +0200

    index page added

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -8,17 +8,20 @@ import { HttpModule } from '@angular/http';
 import { PlayerTwoComponent } from './player-two/player-two.component';
 import { RouterModule, Routes } from '@angular/router';
 import { PlayerOneComponent } from './player-one/player-one.component';
+import { IndexComponent } from './index/index.component';
 
 const appRoutes: Routes = [
   {path: 'playerone', component: PlayerOneComponent},
-  {path: 'playertwo', component: PlayerTwoComponent}
+  {path: 'playertwo', component: PlayerTwoComponent},
+  {path: '**', component: IndexComponent}
 ];
 
 @NgModule({
   declarations: [
     AppComponent,
     PlayerTwoComponent,
-    PlayerOneComponent
+    PlayerOneComponent,
+    IndexComponent
   ],
   imports: [
     HttpModule,
